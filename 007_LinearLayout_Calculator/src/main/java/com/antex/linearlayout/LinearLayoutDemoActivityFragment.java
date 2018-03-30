56d028abd52c49834b0cb25545e3b0f23710e97d
gh-Hannal <952458360@qq.com>
2018-03-30 13:43:29 +0800

    Calculator_UI

---
+++ 007_LinearLayout_Calculator/src/main/java/com/antex/linearlayout/LinearLayoutDemoActivityFragment.java
@@ -0,0 +1,22 @@
+package com.antex.linearlayout;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class LinearLayoutDemoActivityFragment extends Fragment {
+
+    public LinearLayoutDemoActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_linear_layout_demo, container, false);
+    }
+}
