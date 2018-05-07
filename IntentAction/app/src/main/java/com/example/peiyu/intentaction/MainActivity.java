0cf92045231252962030f55f3b80cde058a79bda
gh-Hannal <952458360@qq.com>
2018-05-07 18:03:34 +0800

    实践四

---
+++ IntentAction/app/src/main/java/com/example/peiyu/intentaction/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.peiyu.intentaction;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button bt = findViewById(R.id.phoneCall);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:10086"));
+                startActivity(intent);
+            }
+        });
+    }
+}
